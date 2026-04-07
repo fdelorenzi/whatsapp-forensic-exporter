883db6516064a9d65cec4a683b31d09a4a0ac17c
Francesco de Lorenzi <francesco@leadernova.com>
2026-04-07 11:05:15 +0200

    feat: add media_type, media_path, and is_forwarded fields to MessageDTO
    Extend the data transfer object to carry media attachment metadata
    and forwarded message status across all handlers.

--- exporter/message_dto.py
+++ exporter/message_dto.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 class MessageDTO:
-    def __init__(self, message_id, message_date, sender_nickname, receiver_nickname, sender_number, receiver_number, message_text, message_direction):
+    def __init__(self, message_id, message_date, sender_nickname, receiver_nickname, sender_number, receiver_number, message_text, message_direction, media_type=None, media_path=None, is_forwarded=False):
         self.message_id = message_id
         self.message_date = message_date if isinstance(message_date, datetime) else datetime.strptime(message_date, '%Y-%m-%d %H:%M:%S')
         self.sender_nickname = sender_nickname
@@ -10,3 +10,6 @@ class MessageDTO:
         self.receiver_number = receiver_number
         self.message_text = message_text
         self.message_direction = message_direction
+        self.media_type = media_type
+        self.media_path = media_path
+        self.is_forwarded = is_forwarded
